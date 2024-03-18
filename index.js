4bbe0c6d91e2ce9dc2b91f51ca7fe5e491e68d2f
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-03-19 01:52:29 +0600

    create recommendation api

--- index.js
+++ index.js
@@ -26,12 +26,61 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+   const songsCollection = client.db('musicTracks').collection('songs')
+   const recommendationCollection = client.db('musicTracks').collection('recommendation')
+
+   app.get('/songs',async(req,res)=>{
+    const cursor=songsCollection.find();
+    const result=await cursor.toArray();
+    res.send(result)
+   })
+
+   app.post('/recommendation',async(req,res)=>{
+    const recommendationItem=req.body
+    const result =await recommendationCollection.insertOne(recommendationItem)
+    res.send(result)
+   })
+
+  //  app.get('/recommendation',async(req,res)=>{
+  //   const cursor=recommendationCollection.find()
+  //   const result=await cursor.toArray();
+  //   res.send(result)
+  //  })
+  app.get('/recommendation', async (req, res) => {
+    try {
+      const result = await recommendationCollection.aggregate([
+        {
+          $group: {
+            _id: "$songId",
+            title: { $first: "$title" },
+            artist: { $first: "$artist" },
+            audio_file: { $first: "$audio_file" },
+            image: { $first: "$image" },
+            clickCount: { $sum: "$clickCount" }
+          }
+        },
+        {
+          $sort: { clickCount: 1 }
+        },
+        {
+          $sort: { clickCount: -1 }
+        }
+      ]).toArray();
+  
+      res.send(result);
+    } catch (error) {
+      console.error('Error fetching recommendations:', error);
+      res.status(500).send('Error fetching recommendations');
+    }
+  });
+  
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
